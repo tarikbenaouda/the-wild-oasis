04efac51a2e266374e67df9994c3416669d49887
tarikbenaouda <benaouda210@gmail.com>
2024-11-24 11:21:55 +0100

    Add reusable styled components (Heading, Input, Button)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Row from "./ui/Row";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: aliceblue;
+  padding: 20px;
+`;
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check In & Out</Heading>
+              <Button>Check In</Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">From</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
