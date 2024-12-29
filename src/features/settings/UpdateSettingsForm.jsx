6033b2005d2e33ff2ddfff69c1c14cf4886dabb2
tarikbenaouda <benaouda210@gmail.com>
2024-12-29 14:38:50 +0100

    Add update settings functionality and refactor useSettings hook

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,8 +1,9 @@
-import { useSettings } from "./useSettings";
+import useSettings from "./useSettings";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
+import useUpdateSetting from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
@@ -14,27 +15,57 @@ function UpdateSettingsForm() {
       breakfastPrice,
     } = {},
   } = useSettings();
+  const { updateSetting, isUpdating } = useUpdateSetting();
+
+  function handleUpdate(e) {
+    const map = {
+      "min-nights": "minBookingLength",
+      "max-nights": "maxBookingLength",
+      "max-guests": "maxGuestsPerBooking",
+      "breakfast-price": "breakfastPrice",
+    };
+    const setting = map[e.target.id];
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [setting]: value });
+  }
   if (isLoading) return <Spinner />;
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
+        <Input
+          type="number"
+          id="min-nights"
+          disabled={isUpdating}
+          defaultValue={minBookingLength}
+          onBlur={(e) => handleUpdate(e)}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          disabled={isUpdating}
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e)}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
+          disabled={isUpdating}
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e)}
         />
       </FormRow>
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
+          disabled={isUpdating}
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e)}
         />
       </FormRow>
     </Form>
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import styled from "styled-components";
+import { useQueryClient } from "@tanstack/react-query";
 
 const StyledAppLayout = styled.div`
   display: grid;
